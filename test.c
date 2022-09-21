9e71d609cdfcff90549f59f0ec3f68e0d07d491c
publdaze <78250089+publdaze@users.noreply.github.com>
2022-09-21 21:50:30 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,4 +1,3 @@
-
 #include "stm32f10x.h"
 
 void delay(void)
@@ -20,26 +19,26 @@ int main()
     *((volatile unsigned int *)0x40011404) &= 0xFFFF0FFF; // init port D - 스위치!!! (11 PIN reset)
     *((volatile unsigned int *)0x40011404) |= 0x00008000; // port D input mode
 
-    *((volatile unsigned int *)0x40011800) &= 0xFFFFFF00; // init port E -  (1,2 PIN reset)
+    *((volatile unsigned int *)0x40011800) &= 0xFFFFFF00; // init port E -  (0,1 PIN reset)
     *((volatile unsigned int *)0x40011800) |= 0x00000033; // port E output mode
 
     while (1)
     {
 
-        if (0x1 & ~(*((volatile unsigned int *)0x4001100C) >> 11)) // 스위치 누름 => pd11
+        if (0x1 & ~(*((volatile unsigned int *)0x40011408) >> 11)) // 스위치 누름 => pd11
         {                                                          //모터 정지                                                       // Down 2 bit
-            *((volatile unsigned int *)0x40011810) |= 0x00030000;  // reset PE1,2 -> 0, 0
+            *((volatile unsigned int *)0x40011810) |= 0x00030000;  // reset PE0,1 -> 0, 0
         }
         else if (0x1 & ~(*((volatile unsigned int *)0x40011008) >> 2)) // DOWN -> 반대방향ㅇ
         {
             
-            *((volatile unsigned int *)0x40011810) |= 0x00030000; // reset PE1,2 -> 0, 0
-            *((volatile unsigned int *)0x40011810) |= 0x00000001; // set PE1 -> 1
+            *((volatile unsigned int *)0x40011810) |= 0x00030000; // reset PE0,1 -> 0, 0
+            *((volatile unsigned int *)0x40011810) |= 0x00000001; // set PE0 -> 1
         }
         else if (0x1 & ~(*((volatile unsigned int *)0x40011008) >> 5)) //모터 시계
         {
             *((volatile unsigned int *)0x40011810) |= 0x00030000; // reset PE1,2 -> 0, 0
-            *((volatile unsigned int *)0x40011810) |= 0x00000002;  // set PE2 -> 1
+            *((volatile unsigned int *)0x40011810) |= 0x00000002;  // set PE1 -> 1
         }
     }
 }
