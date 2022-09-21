62bac311dc1a127a776a9ecfa8794f52be31a37b
llddang <77055208+llddang@users.noreply.github.com>
2022-09-21 20:56:55 +0900

    Update test.c

--- test.c
+++ test.c
@@ -11,14 +11,13 @@ void delay(void)
 int main()
 {
     // PC2(조이스틱 down) , PC5(조이스틱 up), PD11(스위치)
-    *((volatile unsigned int *)0x40021018) |= 0x30; // clock enable port C, D
+    *((volatile unsigned int *)0x40021018) |= 0x70; // clock enable port C, D
 
     *((volatile unsigned int *)0x40011000) &= 0xFF0000FF; // init port C
     *((volatile unsigned int *)0x40011000) |= 0x00888800; // port C input mode
 
     *((volatile unsigned int *)0x40011404) &= 0xFFFF0FFF; // init port D - 스위치!!! (11 PIN reset)
     *((volatile unsigned int *)0x40011404) |= 0x00008000; // port D input mode
-
 
     *((volatile unsigned int *)0x40011800) &= 0xFFFFF00F; // init port E -  (1,2 PIN reset)
     *((volatile unsigned int *)0x40011800) |= 0x00000330; // port E output mode
