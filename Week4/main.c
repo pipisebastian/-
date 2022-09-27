c24b48c8d8bb22c45d3000b5a9c412b7a8f57fbf
publdaze <publdaze@naver.com>
2022-09-27 22:35:12 +0900

    Update week4 main.c

--- Week4/main.c
+++ Week4/main.c
@@ -11,7 +11,7 @@ void delay(void)
 int main()
 {
     // PC2(조이스틱 down) , PC5(조이스틱 up), PD11(스위치)
-    *((volatile unsigned int *)0x40021018) |= 0x70; // clock enable port C, D
+    *((volatile unsigned int *)0x40021018) |= 0x70; // clock enable port C, D, E
 
     *((volatile unsigned int *)0x40011000) &= 0xFF0000FF; // init port C
     *((volatile unsigned int *)0x40011000) |= 0x00888800; // port C input mode
@@ -23,25 +23,23 @@ int main()
     *((volatile unsigned int *)0x40011800) |= 0x00000033; // port E output mode
 
     while (1)
-    {   
+    {
         if (0x1 & ~(*((volatile unsigned int *)0x40011408) >> 11)) // 스위치1 누름 => pd11
         {                                                          //모터 정지
-            printf("1"); 
-            *((volatile unsigned int *)0x40011810) |= 0x00030000;  // reset PE0,1 -> 0, 0
+            printf("1");
+            *((volatile unsigned int *)0x40011810) |= 0x00030000; // reset PE0,1 -> 0, 0
         }
         else if (0x1 & ~(*((volatile unsigned int *)0x40011008) >> 2)) // DOWN -> 모터 반시계 방향 회전
-        {    
-            printf("2");  
+        {
+            printf("2");
             *((volatile unsigned int *)0x40011810) |= 0x00030000; // reset PE0,1 -> 0, 0
             *((volatile unsigned int *)0x40011810) |= 0x00000001; // set PE0 -> 1
-            
-            
         }
         else if (0x1 & ~(*((volatile unsigned int *)0x40011008) >> 5)) // Up -> 모터 시계 방향 회전
         {
-            printf("3");  
-            *((volatile unsigned int *)0x40011810) |= 0x00030000;  // reset PE0,1 -> 0, 0
-            *((volatile unsigned int *)0x40011810) |= 0x00000002;  // set PE1 -> 1
+            printf("3");
+            *((volatile unsigned int *)0x40011810) |= 0x00030000; // reset PE0,1 -> 0, 0
+            *((volatile unsigned int *)0x40011810) |= 0x00000002; // set PE1 -> 1
         }
         delay();
     }
