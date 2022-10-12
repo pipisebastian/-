487c2b491cb053b23a2cf08d3c91b7dbc68bc921
이희근 <48307403+Leeheeguen@users.noreply.github.com>
2022-10-12 22:53:47 +0900

    Update main.c

--- Week7/main.c
+++ Week7/main.c
@@ -3,7 +3,6 @@
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_usart.h"
 #include "stm32f10x_rcc.h"
-#include "stdio.h"
 #include "misc.h"
 
 /* function prototype */
@@ -11,177 +10,178 @@ void RCC_Configure(void);
 void GPIO_Configure(void);
 void EXTI_Configure(void);
 void USART1_Init(void);
-void USART2_Init(void);
 void NVIC_Configure(void);
-void USART1_IRQHandler(void);
-void USART2_IRQHandler(void);
+
 void EXTI15_10_IRQHandler(void);
 
 void Delay(void);
 
 void sendDataUART1(uint16_t data);
+
 //---------------------------------------------------------------------------------------------------
 
-void RCC_Configure(void){
+void RCC_Configure(void)
+{
     // TODO: Enable the APB2 peripheral clock using the function 'RCC_APB2PeriphClockCmd'
-
+    
     /* UART TX/RX port clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+    
     /* USART1 clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+    
     /* USART2 clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART2, ENABLE);
-
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+    
     /* Alternate Function IO clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
 
-void GPIO_Configure(void){
+void GPIO_Configure(void)
+{
     GPIO_InitTypeDef GPIO_InitStructure;
 
     // TODO: Initialize the GPIO pins using the structure 'GPIO_InitTypeDef' and the function 'GPIO_Init'
-
-
-    /* USART2 pin setting */
-    // TX -> PA 2
+    
+    /* UART2 pin setting */
+    //TX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // Alternate function output Push-pull로 설정
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-    // RX -> PA 3
+    //RX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU | GPIO_Mode_IPD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-
-    /* USART1 pin setting */
-    // TX -> PA 9
+    
+    /* UART1 pin setting */
+    //TX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // Alternate function output Push-pull로 설정
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-    // RX -> PA 10
+    //RX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU | GPIO_Mode_IPD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
 }
 
-void USART1_Init(void){
+void USART1_Init(void)
+{
     USART_InitTypeDef USART1_InitStructure;
 
     // Enable the USART1 peripheral
     USART_Cmd(USART1, ENABLE);
-
+    
     // TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
-    // USART 초기설정
-
-    USART1_InitStructure.USART_BaudRate = 28800;
+    USART1_InitStructure.USART_BaudRate = 9600;
+    USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx ;
     USART1_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART1_InitStructure.USART_Parity = USART_Parity_No;
     USART1_InitStructure.USART_StopBits = USART_StopBits_1;
     USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx; // Tx, Rx enable
-    USART_Init(USART1, &USART1_InitStructure);
-
+    USART_Init(USART1,&USART1_InitStructure);
+    
     // TODO: Enable the USART1 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
-    // USART Interrupts를 enable!
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
 }
 
-void USART2_Init(void){
+void USART2_Init(void)
+{
     USART_InitTypeDef USART2_InitStructure;
 
-    // Enable the USART1 peripheral
+    // Enable the USART2 peripheral
     USART_Cmd(USART2, ENABLE);
-
+    
     // TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
-    // USART 초기설정
-
-    USART2_InitStructure.USART_BaudRate = 28800;
+    USART2_InitStructure.USART_BaudRate = 9600;
+    USART2_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx ;
     USART2_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART2_InitStructure.USART_Parity = USART_Parity_No;
     USART2_InitStructure.USART_StopBits = USART_StopBits_1;
     USART2_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART2_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx; // Tx, Rx enable
     USART_Init(USART2, &USART2_InitStructure);
-
-    // TODO: Enable the USART1 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
-    // USART Interrupts를 enable!
-    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+    
+    // TODO: Enable the USART2 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
+    USART_ITConfig(USART2, USART_IT_RXNE,ENABLE);
 }
 
-void NVIC_Configure(void){
-    NVIC_InitTypeDef NVIC_InitStructure;
+void NVIC_Configure(void) {
 
+    NVIC_InitTypeDef NVIC_InitStructure;
+    
     // TODO: fill the arg you want
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
 
     // TODO: Initialize the NVIC using the structure 'NVIC_InitTypeDef' and the function 'NVIC_Init'
-    //우선순위 지정 -> 1. 조이스틱 UP // 2. 조이스틱 DOWN // 3. BTN // 4. USART입력
-
+    
     // UART1
     // 'NVIC_EnableIRQ' is only required for USART setting
     NVIC_EnableIRQ(USART1_IRQn);
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;        // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0; // TODO
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
-
+    
     // UART2
     // 'NVIC_EnableIRQ' is only required for USART setting
     NVIC_EnableIRQ(USART2_IRQn);
     NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;        // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0; // TODO
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 }
 
-void USART1_IRQHandler(){
+void USART1_IRQHandler(void) {
     uint16_t word;
-    if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET){
+    if(USART_GetITStatus(USART1,USART_IT_RXNE) != RESET){
         // the most recent received data by the USART1 peripheral
         word = USART_ReceiveData(USART1);
 
-        // send data
+        // TODO implement
         USART_SendData(USART2, word);
-
+        
         // clear 'Read data register not empty' flag
-        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+        USART_ClearITPendingBit(USART1,USART_IT_RXNE);
     }
 }
 
-void USART2_IRQHandler(){
+void USART2_IRQHandler(void) {
     uint16_t word;
-    if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET){
+    if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET){
         // the most recent received data by the USART1 peripheral
         word = USART_ReceiveData(USART2);
 
-        // send data
+        // TODO implement
         USART_SendData(USART1, word);
-
+        
         // clear 'Read data register not empty' flag
         USART_ClearITPendingBit(USART2, USART_IT_RXNE);
     }
 }
 
-int main(void){
+
+void Delay(void) {
+    int i;
+
+    for (i = 0; i < 2000000; i++) {}
+}
+
+int main(void)
+{
+
     SystemInit();
     RCC_Configure();
     GPIO_Configure();
-    EXTI_Configure();
     USART1_Init();
     USART2_Init();
     NVIC_Configure();
-    USART1_IRQHandler();
-    USART2_IRQHandler();
-
-    while (1);
-
+    
+    while(1);
     return 0;
 }
