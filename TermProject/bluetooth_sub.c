d50925962b786b8e0e5f381924ab608609616c92
이희근 <48307403+Leeheeguen@users.noreply.github.com>
2022-11-30 19:15:35 +0900

    Update bluetooth_sub.c

--- TermProject/bluetooth_sub.c
+++ TermProject/bluetooth_sub.c
@@ -8,6 +8,7 @@ uint16_t  u3_rx_buffer[U3_BUFFER_SIZE];
 
 uint32_t  u3_rx_point_head = 0;
 uint32_t  u3_rx_point_tail = 0;
+uint16_t data;
 
 void RCC_Configure(void);
 void GPIO_Configure(void);
@@ -59,8 +60,11 @@ void RCC_Configure(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
     /* USART1 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+    
+    /* USART2 clock enable */
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
-
+    
     /* Alternate Function IO clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
@@ -80,6 +84,36 @@ void GPIO_Configure(void)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
+        /* UART1 pin setting */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    //RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);  
+}
+void USART1_Init(void)
+{
+    USART_InitTypeDef USART1_InitStructure;
+
+    // Enable the USART1 peripheral
+    USART_Cmd(USART1, ENABLE);
+    
+    // TODO: Initialize the USART using the structure 'USART_InitTypeDef' and the function 'USART_Init'
+    USART1_InitStructure.USART_BaudRate = 9600;
+    USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx ;
+    USART1_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART1_InitStructure.USART_Parity = USART_Parity_No;
+    USART1_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_Init(USART1,&USART1_InitStructure);
+    
+    // TODO: Enable the USART1 RX interrupts using the function 'USART_ITConfig' and the argument value 'Receive Data register not empty interrupt'
+    USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
 }
 
 void USART2_Init(void)
@@ -105,6 +139,15 @@ void NVIC_Configure(void) {
     NVIC_InitTypeDef NVIC_InitStructure;
     
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+        
+    // UART1
+    // 'NVIC_EnableIRQ' is only required for USART setting
+    NVIC_EnableIRQ(USART1_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0; // TODO
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
     
     // UART2
     // 'NVIC_EnableIRQ' is only required for USART setting
@@ -115,12 +158,24 @@ void NVIC_Configure(void) {
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 }
+void USART1_IRQHandler(void) {
+    uint16_t word;
+    if(USART_GetITStatus(USART1,USART_IT_RXNE) != RESET){
+        // the most recent received data by the USART1 peripheral
+        word = USART_ReceiveData(USART1);
+        // TODO implement
+        USART_SendData(USART2, word);
+        
+        // clear 'Read data register not empty' flag
+        USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+    }
+}
 
 void USART2_IRQHandler(void) {
-    uint16_t sensor_data[3] = {0}; //가속도 센서 값 3개 input
     if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET){
+        
         // the most recent received data by the USART1 peripheral
-        Uart3_EnQueue(USART_ReceiveData(USART2)); //<-sensor_data의 데이터 형식 선택        
+        Uart3_EnQueue(USART_ReceiveData(USART2)); //<-sensor_data의 데이터 형식 선택 
         // clear 'Read data register not empty' flag
         USART_ClearITPendingBit(USART2, USART_IT_RXNE);
     }
@@ -143,8 +198,9 @@ int main(void)
     
     while(1){
         if(0 == Uart3_Is_Empty()){
-            uint16_t data = Uart3_DeQueue();
-            printf("data: %d", data);
+            data = Uart3_DeQueue();
+            printf("data: %d \n", data);
+            USART_SendData(USART1, data);
         }
     }
     return 0;
