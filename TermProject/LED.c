4c85d8377bfa603081a7c25995dcbc8d7502dab0
llddang <77055208+llddang@users.noreply.github.com>
2022-11-30 19:28:22 +0900

    Update LED.c

--- TermProject/LED.c
+++ TermProject/LED.c
@@ -7,8 +7,10 @@
 #include "stm32f10x_adc.h"
 #include "stm32f10x_dma.h"
 
+// PE2, PE3, PE4를 이용함
+
 void LED_RCC_Configure(void) {
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
 }
 
 void LED_GPIO_Configure(void) {
@@ -16,8 +18,8 @@ void LED_GPIO_Configure(void) {
   // LED Output Mode
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // output mode로
   GPIO_InitStructure.GPIO_Pin=(GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4);// PD2,3,4
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
 }
 
 void LED_Init(void) {
@@ -25,28 +27,14 @@ void LED_Init(void) {
   LED_GPIO_Configure();
 }
 
-//안써욤
-boolean LeftLight = false;
-boolean RightLight = false;
-
-void LeftLight_OnOff(){//main에서 해당 함수 호출시
-}
-
-void RightLight_OnOff(){
-}
-
-void LightOnWhenDark(){
-}
-//안써욤
-
 int main(void)
 {
 		SystemInit();
     LED_Init();
 		 
 		//흰색 키기
-		GPIO_SetBits(GPIOD, GPIO_Pin_2); //파랑색 on
-		GPIO_SetBits(GPIOD, GPIO_Pin_3); //초록색 on
-		GPIO_SetBits(GPIOD, GPIO_Pin_4); // 빨간색 on
+		GPIO_SetBits(GPIOE, GPIO_Pin_2); //파랑색 on
+		GPIO_SetBits(GPIOE, GPIO_Pin_3); //초록색 on
+		GPIO_SetBits(GPIOE, GPIO_Pin_4); // 빨간색 on
     return 0;
 }
