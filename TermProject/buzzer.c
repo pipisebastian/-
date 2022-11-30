fb0c144d1d911869314b279b4c3541ef397b60a2
publdaze <78250089+publdaze@users.noreply.github.com>
2022-11-30 20:10:29 +0900

    Update buzzer.c

--- TermProject/buzzer.c
+++ TermProject/buzzer.c
@@ -28,161 +28,108 @@ uint32_t Music = 0;
 
 void RCC_Configure(void)
 {
-      /* PWM */
-      RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // TIM3
-      /* BUZZER */
-      RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); // PB0
+    /* PWM */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // TIM3
+    /* BUZZER */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); // PB0
 }
 
 void GPIO_Configure(void)
 {
-      /* BUZZER */
-      GPIO_InitTypeDef GPIO_InitStructure;
-      GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-      GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-      GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-      GPIO_Init(GPIOB, &GPIO_InitStructure);
+    /* BUZZER */
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
 void NVIC_Configure(void)
 {
-      NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
-      NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+    NVIC_InitTypeDef NVIC_InitStructure;
 
-      /* Enable the TIM3 gloabal Interrupt */
-      NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-      NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-      NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-      NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-      NVIC_Init(&NVIC_InitStructure);
+    /* Enable the TIM3 gloabal Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
 }
 
 void TIM3_Configure(void)
 {
-      uint16_t prescale = (uint16_t)(SystemCoreClock / 10);
+    uint16_t prescale = (uint16_t)(SystemCoreClock / 10);
 
-      /* Time base configuration */
-      TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-      TIM_TimeBaseStructure.TIM_Period = 10;          // Overflow Interrupt On 10 usec 타이머주기
-      TIM_TimeBaseStructure.TIM_Prescaler = prescale; // Timer/Count2 Clock = 36Mhz / (35 + 1) = 1Mhz = 1 usec
-      TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-      TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; // 카운터모드동작
-      TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+    /* Time base configuration */
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_TimeBaseStructure.TIM_Period = 10;          // Overflow Interrupt On 10 usec 타이머주기
+    TIM_TimeBaseStructure.TIM_Prescaler = prescale; // Timer/Count2 Clock = 36Mhz / (35 + 1) = 1Mhz = 1 usec
+    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; // 카운터모드동작
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
-      /* TIM2 counter enable */
-      TIM_Cmd(TIM3, ENABLE);
+    /* TIM2 counter enable */
+    TIM_Cmd(TIM3, ENABLE);
 
-      /* TIM IT(인터럽트) enable */
-      TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+    /* TIM IT(인터럽트) enable */
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
 }
 
 void TIM3_IRQHandler(void) // 1mS Timer
 {
-  if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
-  {
-    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-
-    Sound++;
-
-    if (Sound >= Music)
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
     {
-      GPIOB->ODR ^= GPIO_Pin_0;
-      Sound = 0;
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+        Sound++;
+
+        if (Sound >= Music)
+        {
+            GPIOB->ODR ^= GPIO_Pin_0;
+            Sound = 0;
+        }
     }
-  }
 }
 
-void Delay_Us(uint32_t us){  
-      if(us>1) {
-            uint32_t count=us*8-6;
-            while(count--); 
-      } else{
-            uint32_t count=2;
-            while(count--); 
-      }
+static void delay(uint32_t nCount)
+{
+    __IO uint16_t i;
+    for (i = 0; i < (nCount * 5); i++)
+    {
+        ;
+    }
+}
+
+void Delay_Us(uint32_t us)
+{
+    if (us > 1)
+    {
+        uint32_t count = us * 8 - 6;
+        while (count--)
+            ;
+    }
+    else
+    {
+        uint32_t count = 2;
+        while (count--)
+            ;
+    }
 }
 
 int main(void)
 {
-      SystemInit();
-      RCC_Configure();
-      GPIO_Configure();
-      TIM3_Configure();
-      NVIC_Configure();
+    SystemInit();
+    RCC_Configure();
+    GPIO_Configure();
+    TIM3_Configure();
+    NVIC_Configure();
 
+    Music = MUSIC_SOL;
+    delay(0.1);
+    Music = MUSIC_REST;
+    delay(0.01);
 
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_RA;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_RA;
-      Delay_Us(100000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_MI;
-      Delay_Us(100000);
-      Delay_Us(100000);
-
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_MI;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_MI;
-      Delay_Us(100000);
-      Music = MUSIC_RE;
-      Delay_Us(100000);
-      Delay_Us(100000);
-
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_RA;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_RA;
-      Delay_Us(100000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_REST;
-      Delay_Us(10000);
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_MI;
-      Delay_Us(100000);
-      Delay_Us(100000);
-
-      Music = MUSIC_SOL;
-      Delay_Us(100000);
-      Music = MUSIC_MI;
-      Delay_Us(100000);
-      Music = MUSIC_RE;
-      Delay_Us(100000);
-      Music = MUSIC_MI;
-      Delay_Us(100000);
-      Music = MUSIC_DO;
-      Delay_Us(100000);
-      Delay_Us(100000);
-
-      TIM_Cmd(TIM3, DISABLE);
-      GPIOB->BRR = GPIO_Pin_0;
-}
+    TIM_Cmd(TIM3, DISABLE);
+    GPIOB->BRR = GPIO_Pin_0;
+} 
