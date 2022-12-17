edccec946dae6104e5607e47516edd39f450181c
publdaze <78250089+publdaze@users.noreply.github.com>
2022-12-17 13:39:43 +0900

    Update buzzer.c

--- TermProject/buzzer.c
+++ TermProject/buzzer.c
@@ -19,6 +19,9 @@ void RCC_Configure(void)
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // TIM3
     /* BUZZER */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); // PB0
+    
+    /* Alternate Function IO clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
 
 void GPIO_Configure(void)
@@ -27,7 +30,7 @@ void GPIO_Configure(void)
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
@@ -131,6 +134,9 @@ int main(void)
         delay();
         delay();
         Music = C5;
+        delay();
+        delay();
+        delay();
     }
 
     TIM_Cmd(TIM3, DISABLE);
