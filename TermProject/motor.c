93d0547a0fcf7872b57b7b3da2be5f6a358184cc
pipisebastian <81643702+pipisebastian@users.noreply.github.com>
2022-12-17 13:59:58 +0900

    Update motor.c
    모터 코드 정리

--- TermProject/motor.c
+++ TermProject/motor.c
@@ -9,122 +9,90 @@
 #include <stdio.h>
 
 /* function prototype */
-void RCC_Configure(void);
-void GPIO_Configure(void);
-void Start(void);
-void Back(void);
-void TurnLeft(void);
-void TurnRight(void);
-void Stop(void);
+void Motor_RCC_Configure(void);
+void Motor_GPIO_Configure(void);
+void Motor_Start(void);
+void Motor_Back(void);
+void Motor_TurnLeft(void);
+void Motor_TurnRight(void);
+void Motor_Stop(void);
 
-int16_t MotorPin[4] = {GPIO_Pin_1, GPIO_Pin_2, GPIO_Pin_3, GPIO_Pin_4};
+int16_t Motor_Pin[4] = {GPIO_Pin_10, GPIO_Pin_11, GPIO_Pin_12, GPIO_Pin_13};
 
-void RCC_Configure(void)
+void Motor_RCC_Configure(void)
 {
-    /* Alternate Function IO clock enable */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
 }
 
-void GPIO_Configure(void) //
+void Motor_GPIO_Configure(void) //
 {
-    // PE1,2,3,4 -> IN1,IN2,IN3,IN4
-    // PE5,6 -> ENA, ENB
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12 | GPIO_Pin_12;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // output mode로
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Init(GPIOE, &GPIO_InitStructure);
 }
-void Start(void)
+void Motor_Start(void)
 {
     GPIO_SetBits(GPIOE, MotorPin[0]);
     GPIO_ResetBits(GPIOE, MotorPin[1]);
     GPIO_ResetBits(GPIOE, MotorPin[2]);
     GPIO_SetBits(GPIOE, MotorPin[3]);
-
-    // GPIO_WriteBit(GPIOE, MotorPin[0], Bit_SET);
-    // GPIO_WriteBit(GPIOE, MotorPin[1], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[2], Bit_SET);
-    // GPIO_WriteBit(GPIOE, MotorPin[3], Bit_RESET);
 }
 
-void Back(void)
+void Motor_Back(void)
 {
     GPIO_ResetBits(GPIOE, MotorPin[0]);
     GPIO_SetBits(GPIOE, MotorPin[1]);
     GPIO_SetBits(GPIOE, MotorPin[2]);
     GPIO_ResetBits(GPIOE, MotorPin[3]);
-
-    //     GPIO_WriteBit(GPIOE, MotorPin[0], Bit_RESET);
-    //     GPIO_WriteBit(GPIOE, MotorPin[1], Bit_SET);
-    //     GPIO_WriteBit(GPIOE, MotorPin[2], Bit_RESET);
-    //     GPIO_WriteBit(GPIOE, MotorPin[3], Bit_SET);
-    //
 }
 
-void TurnLeft(void)
+void Motor_TurnLeft(void)
 {
-    printf("1");
     GPIO_ResetBits(GPIOE, MotorPin[0]);
     GPIO_SetBits(GPIOE, MotorPin[1]);
     GPIO_ResetBits(GPIOE, MotorPin[2]);
     GPIO_SetBits(GPIOE, MotorPin[3]);
-    printf("2");
-    // GPIO_WriteBit(GPIOE, MotorPin[0], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[1], Bit_SET);
-    // GPIO_WriteBit(GPIOE, MotorPin[2], Bit_SET);
-    // GPIO_WriteBit(GPIOE, MotorPin[3], Bit_RESET);
 }
 
-void TurnRight(void)
+void Motor_TurnRight(void)
 {
     GPIO_SetBits(GPIOE, MotorPin[0]);
     GPIO_ResetBits(GPIOE, MotorPin[1]);
     GPIO_SetBits(GPIOE, MotorPin[2]);
     GPIO_ResetBits(GPIOE, MotorPin[3]);
-    // GPIO_WriteBit(GPIOE, MotorPin[0], Bit_SET);
-    // GPIO_WriteBit(GPIOE, MotorPin[1], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[2], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[3], Bit_SET);
 }
 
-void Stop(void)
+void Motor_Stop(void)
 {
     GPIO_ResetBits(GPIOE, MotorPin[0]);
     GPIO_ResetBits(GPIOE, MotorPin[1]);
     GPIO_ResetBits(GPIOE, MotorPin[2]);
     GPIO_ResetBits(GPIOE, MotorPin[3]);
-    // GPIO_WriteBit(GPIOE, MotorPin[0], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[1], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[2], Bit_RESET);
-    // GPIO_WriteBit(GPIOE, MotorPin[3], Bit_RESET);
 }
 
-void ControlMotorSpeed(int speed)
+void Motor_Init(void)
 {
+    Motor_RCC_Configure();
+    Motor_GPIO_Configure();
 
-    // pwm조정 음냐링
+    Motor_Start(void);
+    Motor_Back(void);
+    Motor_TurnLeft(void);
+    Motor_TurnRight(void);
+    Motor_Stop(void);
 }
 
 int main(void)
 {
 
     SystemInit();
-    RCC_Configure();
-    GPIO_Configure();
-
-    // GPIO_WriteBit(GPIOE, GPIO_Pin_5, Bit_SET);
-    // GPIO_WriteBit(GPIOE, GPIO_Pin_6, Bit_SET);
+    Motor_Init(void);
 
     while (1)
     {
-        //속도 느리게
-        // ControlMotorSpeed(50);
-        printf("0");
         TurnLeft();
-        printf("4");
     }
     return 0;
 }
